ba238f25591f1c4bd823da56cb4c919f131caa6a
Jay Paik <jay@alchemy.com>
2024-01-23 18:34:02 -0500

    refactor: move FunctionReference to IPluginManager

--- src/interfaces/IPluginManager.sol
+++ src/interfaces/IPluginManager.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: CC0-1.0
 pragma solidity ^0.8.19;
 
-import {FunctionReference} from "../helpers/FunctionReferenceLib.sol";
+type FunctionReference is bytes21;
 
 /// @title Plugin Manager Interface
 interface IPluginManager {
