a8c6d36ea3bc35b360503eb426a8ce5d37763cc9
sm-stack <jeon0015@naver.com>
2024-02-15 22:09:26 +0900

    Add getSessionKeysAndSelectors function

--- src/samples/plugins/interfaces/ISessionKeyPlugin.sol
+++ src/samples/plugins/interfaces/ISessionKeyPlugin.sol
@@ -9,90 +9,100 @@ interface IModularSessionKeyPlugin {
         USER_OP_VALIDATION_TEMPORARY_OWNER
     }
 
-    /// @notice This event is emitted when a temporary owner is added to the account.
-    /// @param account The account whose temporary owner is updated.
-    /// @param tempOwner The address of the temporary owner.
-    /// @param selector The selector of the function that the temporary owner is allowed to call.
+    /// @notice This event is emitted when a session key is added to the account.
+    /// @param account The account whose session key is updated.
+    /// @param sessionKey The address of the session key.
+    /// @param selector The selector of the function that the session key is allowed to call.
     /// @param _after The time after which the owner is valid.
     /// @param _until The time until which the owner is valid.
     event SessionKeyAdded(
-        address indexed account, address indexed tempOwner, bytes4 selector, uint48 _after, uint48 _until
+        address indexed account, address indexed sessionKey, bytes4 selector, uint48 _after, uint48 _until
     );
 
-    /// @notice This event is emitted when a temporary owner is removed from the account.
-    /// @param account The account whose temporary owner is updated.
-    /// @param tempOwner The address of the temporary owner.
-    /// @param selector The selector of the function that the temporary owner is allowed to call.
-    event SessionKeyRemoved(address indexed account, address indexed tempOwner, bytes4 selector);
+    /// @notice This event is emitted when a session key is removed from the account.
+    /// @param account The account whose session key is updated.
+    /// @param sessionKey The address of the session key.
+    /// @param selector The selector of the function that the session key is allowed to call.
+    event SessionKeyRemoved(address indexed account, address indexed sessionKey, bytes4 selector);
 
-    /// @notice This event is emitted when temporary owners are added to the account.
-    /// @param account The account whose temporary owners are updated.
-    /// @param tempOwners The addresses of the temporary owners.
-    /// @param selectors The selectors of the functions that the temporary owners are allowed to call.
+    /// @notice This event is emitted when session keys are added to the account.
+    /// @param account The account whose session keys are updated.
+    /// @param sessionKeys The addresses of the session keys.
+    /// @param selectors The selectors of the functions that the session keys are allowed to call.
     /// @param _afters The times after which the owners are valid.
     /// @param _untils The times until which the owners are valid.
     event SessionKeysAdded(
         address indexed account,
-        address[] indexed tempOwners,
+        address[] indexed sessionKeys,
         bytes4[] selectors,
         uint48[] _afters,
         uint48[] _untils
     );
 
-    /// @notice This event is emitted when temporary owners are removed from the account.
-    /// @param account The account whose temporary owners are updated.
-    /// @param tempOwners The addresses of the temporary owners.
-    /// @param selectors The selectors of the functions that the temporary owners are allowed to call.
-    event SessionKeysRemoved(address indexed account, address[] indexed tempOwners, bytes4[] selectors);
+    /// @notice This event is emitted when session keys are removed from the account.
+    /// @param account The account whose session keys are updated.
+    /// @param sessionKeys The addresses of the session keys.
+    /// @param selectors The selectors of the functions that the session keys are allowed to call.
+    event SessionKeysRemoved(address indexed account, address[] indexed sessionKeys, bytes4[] selectors);
 
+    error InvalidSignature();
     error NotAuthorized();
     error WrongTimeRangeForSession();
     error WrongDataLength();
 
-    /// @notice Add a temporary owner to the account.
+    /// @notice Add a session key to the account.
     /// @dev This function is installed on the account as part of plugin installation, and should
     /// only be called from an account. The function selector installed by a child session key plugin
     /// is passed as a parameter, which enforces its own permissions on the calls it can make.
-    /// @param tempOwner The address of the temporary owner.
-    /// @param allowedSelector The selector of the function that the temporary owner is allowed to call.
+    /// @param sessionKey The address of the session key.
+    /// @param allowedSelector The selector of the function that the session key is allowed to call.
     /// @param _after The time after which the owner is valid.
     /// @param _until The time until which the owner is valid.
-    function addSessionKey(address tempOwner, bytes4 allowedSelector, uint48 _after, uint48 _until) external;
+    function addSessionKey(address sessionKey, bytes4 allowedSelector, uint48 _after, uint48 _until) external;
 
-    /// @notice Remove a temporary owner from the account.
+    /// @notice Remove a session key from the account.
     /// @dev This function is installed on the account as part of plugin installation, and should
     /// only be called from an account.
-    /// @param tempOwner The address of the temporary owner.
-    /// @param allowedSelector The selector of the function that the temporary owner is allowed to call.
-    function removeSessionKey(address tempOwner, bytes4 allowedSelector) external;
+    /// @param sessionKey The address of the session key.
+    /// @param allowedSelector The selector of the function that the session key is allowed to call.
+    function removeSessionKey(address sessionKey, bytes4 allowedSelector) external;
 
-    /// @notice Add temporary owners to the account.
+    /// @notice Add session keys to the account.
     /// @dev This function is installed on the account as part of plugin installation, and should
     /// only be called from an account.
-    /// @param tempOwners The addresses of the temporary owners.
-    /// @param allowedSelectors The selectors of the functions that the temporary owners are allowed to call.
+    /// @param sessionKeys The addresses of the session keys.
+    /// @param allowedSelectors The selectors of the functions that the session keys are allowed to call.
     /// @param _afters The times after which the owners are valid.
     /// @param _untils The times until which the owners are valid.
     function addSessionKeyBatch(
-        address[] calldata tempOwners,
+        address[] calldata sessionKeys,
         bytes4[] calldata allowedSelectors,
         uint48[] calldata _afters,
         uint48[] calldata _untils
     ) external;
 
-    /// @notice Remove temporary owners from the account.
+    /// @notice Remove session keys from the account.
     /// @dev This function is installed on the account as part of plugin installation, and should
     /// only be called from an account.
-    /// @param tempOwners The addresses of the temporary owners.
-    /// @param allowedSelectors The selectors of the functions that the temporary owners are allowed to call.
-    function removeSessionKeyBatch(address[] calldata tempOwners, bytes4[] calldata allowedSelectors) external;
+    /// @param sessionKeys The addresses of the session keys.
+    /// @param allowedSelectors The selectors of the functions that the session keys are allowed to call.
+    function removeSessionKeyBatch(address[] calldata sessionKeys, bytes4[] calldata allowedSelectors) external;
 
-    /// @notice Get Session data for a given account and temporary owner.
+    /// @notice Get Session data for a given account and session key.
     /// @param account The account to get session data for.
-    /// @param tempOwner The address of the temporary owner.
-    /// @param allowedSelector The selector of the function that the temporary owner is allowed to call.
-    function getSessionDuration(address account, address tempOwner, bytes4 allowedSelector)
+    /// @param sessionKey The address of the session key.
+    /// @param allowedSelector The selector of the function that the session key is allowed to call.
+    function getSessionDuration(address account, address sessionKey, bytes4 allowedSelector)
         external
         view
         returns (uint48 _after, uint48 _until);
+
+    /// @notice Get all session keys and selectors for a given account.
+    /// @param account The account to get session keys and selectors for.
+    /// @return sessionKeys The addresses of the session keys.
+    /// @return selectors The selectors of the functions that the session keys are allowed to call.
+    function getSessionKeysAndSelectors(address account)
+        external
+        view
+        returns (address[] memory sessionKeys, bytes4[] memory selectors);
 }
