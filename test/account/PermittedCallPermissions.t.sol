3959430f8e46274bdee40fbac6f250feb6960247
adam <adam@alchemy.com>
2024-06-04 14:32:10 -0400

    remove executeFromPlugin & efp external

---
+++ test/account/PermittedCallPermissions.t.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+import {UpgradeableModularAccount} from "../../src/account/UpgradeableModularAccount.sol";
+import {FunctionReference} from "../../src/helpers/FunctionReferenceLib.sol";
+
+import {ResultCreatorPlugin} from "../mocks/plugins/ReturnDataPluginMocks.sol";
+import {PermittedCallerPlugin} from "../mocks/plugins/PermittedCallMocks.sol";
+import {AccountTestBase} from "../utils/AccountTestBase.sol";
+
+contract PermittedCallPermissionsTest is AccountTestBase {
+    ResultCreatorPlugin public resultCreatorPlugin;
+
+    PermittedCallerPlugin public permittedCallerPlugin;
+
+    function setUp() public {
+        _transferOwnershipToTest();
+        resultCreatorPlugin = new ResultCreatorPlugin();
+
+        // Initialize the permitted caller plugins, which will attempt to use the permissions system to authorize
+        // calls.
+        permittedCallerPlugin = new PermittedCallerPlugin();
+
+        // Add the result creator plugin to the account
+        bytes32 resultCreatorManifestHash = keccak256(abi.encode(resultCreatorPlugin.pluginManifest()));
+        vm.prank(address(entryPoint));
+        account1.installPlugin({
+            plugin: address(resultCreatorPlugin),
+            manifestHash: resultCreatorManifestHash,
+            pluginInstallData: "",
+            dependencies: new FunctionReference[](0)
+        });
+        // Add the permitted caller plugin to the account
+        bytes32 permittedCallerManifestHash = keccak256(abi.encode(permittedCallerPlugin.pluginManifest()));
+        vm.prank(address(entryPoint));
+        account1.installPlugin({
+            plugin: address(permittedCallerPlugin),
+            manifestHash: permittedCallerManifestHash,
+            pluginInstallData: "",
+            dependencies: new FunctionReference[](0)
+        });
+    }
+
+    function test_permittedCall_Allowed() public {
+        bytes memory result = PermittedCallerPlugin(address(account1)).usePermittedCallAllowed();
+        bytes32 actual = abi.decode(result, (bytes32));
+
+        assertEq(actual, keccak256("bar"));
+    }
+
+    function test_permittedCall_NotAllowed() public {
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                UpgradeableModularAccount.ExecFromPluginNotPermitted.selector,
+                address(permittedCallerPlugin),
+                ResultCreatorPlugin.bar.selector
+            )
+        );
+        PermittedCallerPlugin(address(account1)).usePermittedCallNotAllowed();
+    }
+}
