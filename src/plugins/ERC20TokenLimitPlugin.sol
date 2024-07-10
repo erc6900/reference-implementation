db9fc97786fc99cf4477bb683afcf495a6cfbc3d
howydev <132113803+howydev@users.noreply.github.com>
2024-07-10 17:52:11 -0400

    fix: test, lint

--- src/plugins/ERC20TokenLimitPlugin.sol
+++ src/plugins/ERC20TokenLimitPlugin.sol
@@ -5,7 +5,11 @@ import {PackedUserOperation} from "@eth-infinitism/account-abstraction/interface
 import {UserOperationLib} from "@eth-infinitism/account-abstraction/core/UserOperationLib.sol";
 import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {SetValue, AssociatedLinkedListSet, AssociatedLinkedListSetLib} from "@modular-account-libs/libraries/AssociatedLinkedListSetLib.sol";
+import {
+    SetValue,
+    AssociatedLinkedListSet,
+    AssociatedLinkedListSetLib
+} from "@modular-account-libs/libraries/AssociatedLinkedListSetLib.sol";
 
 import {PluginManifest, PluginMetadata} from "../interfaces/IPlugin.sol";
 import {IStandardExecutor, Call} from "../interfaces/IStandardExecutor.sol";
@@ -41,37 +45,9 @@ contract ERC20TokenLimitPlugin is BasePlugin, IExecutionHook {
     error ExceededNumberOfEntities();
     error SelectorNotAllowed();
 
-    function getTokensForAccount(address account) external view returns (address[] memory tokens) {
-        SetValue[] memory set = _tokenList.getAll(account);
-        tokens = new address[](set.length);
-        for (uint256 i = 0; i < tokens.length; i++) {
-            tokens[i] = address(bytes20(bytes32(SetValue.unwrap(set[i]))));
-        }
-        return tokens;
-    }
-
     function updateLimits(uint8 functionId, address token, uint256 newLimit) external {
         _tokenList.tryAdd(msg.sender, SetValue.wrap(bytes30(bytes20(token))));
         limits[functionId][token][msg.sender] = newLimit;
-    }
-
-    function _decrementLimit(uint8 functionId, address token, bytes memory innerCalldata) internal {
-        bytes4 selector;
-        uint256 spend;
-        assembly {
-            selector := mload(add(innerCalldata, 32)) // 0:32 is arr len, 32:36 is selector
-            spend := mload(add(innerCalldata, 68)) // 36:68 is recipient, 68:100 is spend
-        }
-        // transfer.selector = 0xa9059cbb. Using `transfer.selector` causes solhint to throw a warning
-        if (selector == 0xa9059cbb || selector == IERC20.approve.selector) {
-            uint256 limit = limits[functionId][token][msg.sender];
-            if (spend > limit) {
-                revert ExceededTokenLimit();
-            }
-            limits[functionId][token][msg.sender] = limit - spend;
-        } else {
-            revert SelectorNotAllowed();
-        }
     }
 
     /// @inheritdoc IExecutionHook
@@ -99,14 +75,10 @@ contract ERC20TokenLimitPlugin is BasePlugin, IExecutionHook {
         return "";
     }
 
-    /// @inheritdoc IExecutionHook
-    function postExecutionHook(uint8, bytes calldata) external pure override {
-        revert NotImplemented();
-    }
-
     /// @inheritdoc IPlugin
     function onInstall(bytes calldata data) external override {
-        (uint8 startFunctionId, ERC20SpendLimit[] memory spendLimits) = abi.decode(data, (uint8, ERC20SpendLimit[]));
+        (uint8 startFunctionId, ERC20SpendLimit[] memory spendLimits) =
+            abi.decode(data, (uint8, ERC20SpendLimit[]));
 
         if (startFunctionId + spendLimits.length > type(uint8).max) {
             revert ExceededNumberOfEntities();
@@ -126,6 +98,20 @@ contract ERC20TokenLimitPlugin is BasePlugin, IExecutionHook {
         delete limits[functionId][token][msg.sender];
     }
 
+    function getTokensForAccount(address account) external view returns (address[] memory tokens) {
+        SetValue[] memory set = _tokenList.getAll(account);
+        tokens = new address[](set.length);
+        for (uint256 i = 0; i < tokens.length; i++) {
+            tokens[i] = address(bytes20(bytes32(SetValue.unwrap(set[i]))));
+        }
+        return tokens;
+    }
+
+    /// @inheritdoc IExecutionHook
+    function postExecutionHook(uint8, bytes calldata) external pure override {
+        revert NotImplemented();
+    }
+
     /// @inheritdoc IPlugin
     // solhint-disable-next-line no-empty-blocks
     function pluginManifest() external pure override returns (PluginManifest memory) {}
@@ -142,12 +128,27 @@ contract ERC20TokenLimitPlugin is BasePlugin, IExecutionHook {
         return metadata;
     }
 
-    // ┏━━━━━━━━━━━━━━━┓
-    // ┃    EIP-165    ┃
-    // ┗━━━━━━━━━━━━━━━┛
-
     /// @inheritdoc BasePlugin
     function supportsInterface(bytes4 interfaceId) public view override(BasePlugin, IERC165) returns (bool) {
         return super.supportsInterface(interfaceId);
+    }
+
+    function _decrementLimit(uint8 functionId, address token, bytes memory innerCalldata) internal {
+        bytes4 selector;
+        uint256 spend;
+        assembly {
+            selector := mload(add(innerCalldata, 32)) // 0:32 is arr len, 32:36 is selector
+            spend := mload(add(innerCalldata, 68)) // 36:68 is recipient, 68:100 is spend
+        }
+        // transfer.selector = 0xa9059cbb. Using `transfer.selector` causes solhint to throw a warning
+        if (selector == 0xa9059cbb || selector == IERC20.approve.selector) {
+            uint256 limit = limits[functionId][token][msg.sender];
+            if (spend > limit) {
+                revert ExceededTokenLimit();
+            }
+            limits[functionId][token][msg.sender] = limit - spend;
+        } else {
+            revert SelectorNotAllowed();
+        }
     }
 }
