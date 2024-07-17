dc9ed7934fd36312d4d0b0870b1d760633af76a2
fangting-alchemy <119372438+fangting-alchemy@users.noreply.github.com>
2024-07-16 17:23:58 -0700

    [4/n] validation series - delete SingleOwnerPlugin (#97)

--- test/utils/AccountTestBase.sol
+++ test/utils/AccountTestBase.sol
@@ -9,39 +9,36 @@ import {SingleSignerValidation} from "../../src/plugins/validation/SingleSignerV
 import {PluginEntity, PluginEntityLib} from "../../src/helpers/PluginEntityLib.sol";
 import {IStandardExecutor, Call} from "../../src/interfaces/IStandardExecutor.sol";
 import {UpgradeableModularAccount} from "../../src/account/UpgradeableModularAccount.sol";
-import {SingleOwnerPlugin} from "../../src/plugins/owner/SingleOwnerPlugin.sol";
 
 import {OptimizedTest} from "./OptimizedTest.sol";
-import {TEST_DEFAULT_OWNER_FUNCTION_ID as EXT_CONST_TEST_DEFAULT_OWNER_FUNCTION_ID} from "./TestConstants.sol";
+import {TEST_DEFAULT_VALIDATION_ENTITY_ID as EXT_CONST_TEST_DEFAULT_VALIDATION_ENTITY_ID} from
+    "./TestConstants.sol";
 
 import {SingleSignerFactoryFixture} from "../mocks/SingleSignerFactoryFixture.sol";
-import {MSCAFactoryFixture} from "../mocks/MSCAFactoryFixture.sol";
 
 /// @dev This contract handles common boilerplate setup for tests using UpgradeableModularAccount with
-/// SingleOwnerPlugin.
+/// SingleSignerValidation.
 abstract contract AccountTestBase is OptimizedTest {
     using PluginEntityLib for PluginEntity;
     using MessageHashUtils for bytes32;
 
     EntryPoint public entryPoint;
     address payable public beneficiary;
-    SingleOwnerPlugin public singleOwnerPlugin;
-    MSCAFactoryFixture public factory;
 
     SingleSignerValidation public singleSignerValidation;
-    SingleSignerFactoryFixture public singleSignerFactory;
+    SingleSignerFactoryFixture public factory;
 
     address public owner1;
     uint256 public owner1Key;
     UpgradeableModularAccount public account1;
 
-    PluginEntity internal _ownerValidation;
+    PluginEntity internal _signerValidation;
 
     uint8 public constant SELECTOR_ASSOCIATED_VALIDATION = 0;
     uint8 public constant GLOBAL_VALIDATION = 1;
 
     // Re-declare the constant to prevent derived test contracts from having to import it
-    uint32 public constant TEST_DEFAULT_OWNER_FUNCTION_ID = EXT_CONST_TEST_DEFAULT_OWNER_FUNCTION_ID;
+    uint32 public constant TEST_DEFAULT_VALIDATION_ENTITY_ID = EXT_CONST_TEST_DEFAULT_VALIDATION_ENTITY_ID;
 
     uint256 public constant CALL_GAS_LIMIT = 100000;
     uint256 public constant VERIFICATION_GAS_LIMIT = 1200000;
@@ -56,15 +53,14 @@ abstract contract AccountTestBase is OptimizedTest {
         (owner1, owner1Key) = makeAddrAndKey("owner1");
         beneficiary = payable(makeAddr("beneficiary"));
 
-        singleOwnerPlugin = _deploySingleOwnerPlugin();
-        factory = new MSCAFactoryFixture(entryPoint, singleOwnerPlugin);
         singleSignerValidation = _deploySingleSignerValidation();
-        singleSignerFactory = new SingleSignerFactoryFixture(entryPoint, singleSignerValidation);
+        factory = new SingleSignerFactoryFixture(entryPoint, singleSignerValidation);
 
         account1 = factory.createAccount(owner1, 0);
         vm.deal(address(account1), 100 ether);
 
-        _ownerValidation = PluginEntityLib.pack(address(singleOwnerPlugin), TEST_DEFAULT_OWNER_FUNCTION_ID);
+        _signerValidation =
+            PluginEntityLib.pack(address(singleSignerValidation), TEST_DEFAULT_VALIDATION_ENTITY_ID);
     }
 
     function _runExecUserOp(address target, bytes memory callData) internal {
@@ -107,7 +103,7 @@ abstract contract AccountTestBase is OptimizedTest {
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(owner1Key, userOpHash.toEthSignedMessageHash());
 
         userOp.signature = _encodeSignature(
-            PluginEntityLib.pack(address(singleOwnerPlugin), TEST_DEFAULT_OWNER_FUNCTION_ID),
+            PluginEntityLib.pack(address(singleSignerValidation), TEST_DEFAULT_VALIDATION_ENTITY_ID),
             GLOBAL_VALIDATION,
             abi.encodePacked(r, s, v)
         );
@@ -160,7 +156,7 @@ abstract contract AccountTestBase is OptimizedTest {
         account1.executeWithAuthorization(
             callData,
             _encodeSignature(
-                PluginEntityLib.pack(address(singleOwnerPlugin), TEST_DEFAULT_OWNER_FUNCTION_ID),
+                PluginEntityLib.pack(address(singleSignerValidation), TEST_DEFAULT_VALIDATION_ENTITY_ID),
                 GLOBAL_VALIDATION,
                 ""
             )
@@ -175,7 +171,7 @@ abstract contract AccountTestBase is OptimizedTest {
         account1.executeWithAuthorization(
             callData,
             _encodeSignature(
-                PluginEntityLib.pack(address(singleOwnerPlugin), TEST_DEFAULT_OWNER_FUNCTION_ID),
+                PluginEntityLib.pack(address(singleSignerValidation), TEST_DEFAULT_VALIDATION_ENTITY_ID),
                 GLOBAL_VALIDATION,
                 ""
             )
@@ -189,15 +185,15 @@ abstract contract AccountTestBase is OptimizedTest {
             abi.encodeCall(
                 account1.execute,
                 (
-                    address(singleOwnerPlugin),
+                    address(singleSignerValidation),
                     0,
                     abi.encodeCall(
-                        SingleOwnerPlugin.transferOwnership, (TEST_DEFAULT_OWNER_FUNCTION_ID, address(this))
+                        SingleSignerValidation.transferSigner, (TEST_DEFAULT_VALIDATION_ENTITY_ID, address(this))
                     )
                 )
             ),
             _encodeSignature(
-                PluginEntityLib.pack(address(singleOwnerPlugin), TEST_DEFAULT_OWNER_FUNCTION_ID),
+                PluginEntityLib.pack(address(singleSignerValidation), TEST_DEFAULT_VALIDATION_ENTITY_ID),
                 GLOBAL_VALIDATION,
                 ""
             )
