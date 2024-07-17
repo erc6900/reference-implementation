44f80f46159995591f37d7f62dffe8d5eb97d3d4
Zer0dot <zer0dot.dev@gmail.com>
2024-07-17 23:00:14 +0800

    Allow direct plugin calls with validation & permission hooks (#90)

--- src/account/PluginManager2.sol
+++ src/account/PluginManager2.sol
@@ -7,7 +7,7 @@ import {IPlugin} from "../interfaces/IPlugin.sol";
 import {PluginEntity, ValidationConfig} from "../interfaces/IPluginManager.sol";
 import {PluginEntityLib} from "../helpers/PluginEntityLib.sol";
 import {ValidationConfigLib} from "../helpers/ValidationConfigLib.sol";
-import {ValidationData, getAccountStorage, toSetValue, toPluginEntity} from "./AccountStorage.sol";
+import {ValidationData, getAccountStorage, toSetValue} from "./AccountStorage.sol";
 import {ExecutionHook} from "../interfaces/IAccountLoupe.sol";
 
 // Temporary additional functions for a user-controlled install flow for validation functions.
@@ -17,6 +17,7 @@ abstract contract PluginManager2 {
 
     // Index marking the start of the data for the validation function.
     uint8 internal constant _RESERVED_VALIDATION_DATA_INDEX = 255;
+    uint32 internal constant _SELF_PERMIT_VALIDATION_FUNCTIONID = type(uint32).max;
 
     error PreValidationAlreadySet(PluginEntity validationFunction, PluginEntity preValidationFunction);
     error ValidationAlreadySet(bytes4 selector, PluginEntity validationFunction);
@@ -32,7 +33,7 @@ abstract contract PluginManager2 {
         bytes memory permissionHooks
     ) internal {
         ValidationData storage _validationData =
-            getAccountStorage().validationData[validationConfig.functionReference()];
+            getAccountStorage().validationData[validationConfig.pluginEntity()];
 
         if (preValidationHooks.length > 0) {
             (PluginEntity[] memory preValidationFunctions, bytes[] memory initDatas) =
@@ -63,7 +64,7 @@ abstract contract PluginManager2 {
                 ExecutionHook memory permissionFunction = permissionFunctions[i];
 
                 if (!_validationData.permissionHooks.add(toSetValue(permissionFunction))) {
-                    revert PermissionAlreadySet(validationConfig.functionReference(), permissionFunction);
+                    revert PermissionAlreadySet(validationConfig.pluginEntity(), permissionFunction);
                 }
 
                 if (initDatas[i].length > 0) {
@@ -73,19 +74,21 @@ abstract contract PluginManager2 {
             }
         }
 
-        _validationData.isGlobal = validationConfig.isGlobal();
-        _validationData.isSignatureValidation = validationConfig.isSignatureValidation();
-
         for (uint256 i = 0; i < selectors.length; ++i) {
             bytes4 selector = selectors[i];
             if (!_validationData.selectors.add(toSetValue(selector))) {
-                revert ValidationAlreadySet(selector, validationConfig.functionReference());
+                revert ValidationAlreadySet(selector, validationConfig.pluginEntity());
             }
         }
 
-        if (installData.length > 0) {
-            address plugin = validationConfig.plugin();
-            IPlugin(plugin).onInstall(installData);
+        if (validationConfig.entityId() != _SELF_PERMIT_VALIDATION_FUNCTIONID) {
+            // Only allow global validations and signature validations if they're not direct-call validations.
+
+            _validationData.isGlobal = validationConfig.isGlobal();
+            _validationData.isSignatureValidation = validationConfig.isSignatureValidation();
+            if (installData.length > 0) {
+                IPlugin(validationConfig.plugin()).onInstall(installData);
+            }
         }
     }
 
@@ -120,12 +123,12 @@ abstract contract PluginManager2 {
 
             // Clear permission hooks
             EnumerableSet.Bytes32Set storage permissionHooks = _validationData.permissionHooks;
-            uint256 i = 0;
-            while (permissionHooks.length() > 0) {
-                PluginEntity permissionHook = toPluginEntity(permissionHooks.at(0));
-                permissionHooks.remove(toSetValue(permissionHook));
-                (address permissionHookPlugin,) = PluginEntityLib.unpack(permissionHook);
-                IPlugin(permissionHookPlugin).onUninstall(permissionHookUninstallDatas[i++]);
+            uint256 len = permissionHooks.length();
+            for (uint256 i = 0; i < len; ++i) {
+                bytes32 permissionHook = permissionHooks.at(0);
+                permissionHooks.remove(permissionHook);
+                address permissionHookPlugin = address(uint160(bytes20(permissionHook)));
+                IPlugin(permissionHookPlugin).onUninstall(permissionHookUninstallDatas[i]);
             }
         }
         delete _validationData.preValidationHooks;
