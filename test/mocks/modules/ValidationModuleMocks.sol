c41cc7ca74abd34225a901a37eae35dcb3e930f8
Adam Egyed <5456061+adamegyed@users.noreply.github.com>
2024-07-23 13:04:52 -0400

    feat: [v0.8-develop] merge plugin manager contracts 2/N (#102)

--- test/mocks/modules/ValidationModuleMocks.sol
+++ test/mocks/modules/ValidationModuleMocks.sol
@@ -118,7 +118,7 @@ contract MockUserOpValidationModule is MockBaseUserOpValidationModule {
         manifest.validationFunctions = new ManifestValidation[](1);
         manifest.validationFunctions[0] = ManifestValidation({
             entityId: uint32(EntityId.USER_OP_VALIDATION),
-            isDefault: false,
+            isGlobal: false,
             isSignatureValidation: false,
             selectors: validationSelectors
         });
@@ -161,7 +161,7 @@ contract MockUserOpValidation1HookModule is MockBaseUserOpValidationModule {
         manifest.validationFunctions = new ManifestValidation[](2);
         manifest.validationFunctions[0] = ManifestValidation({
             entityId: uint32(EntityId.USER_OP_VALIDATION),
-            isDefault: false,
+            isGlobal: false,
             isSignatureValidation: false,
             selectors: validationSelectors
         });
@@ -207,7 +207,7 @@ contract MockUserOpValidation2HookModule is MockBaseUserOpValidationModule {
         manifest.validationFunctions = new ManifestValidation[](1);
         manifest.validationFunctions[0] = ManifestValidation({
             entityId: uint32(EntityId.USER_OP_VALIDATION),
-            isDefault: false,
+            isGlobal: false,
             isSignatureValidation: false,
             selectors: validationSelectors
         });
