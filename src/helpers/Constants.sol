8bf2511fa73c3067277831a985b423703a95f87e
Jay Paik <jay@alchemy.com>
2024-10-15 16:01:42 -0400

    fix: update constant name for MAX_PRE_VALIDATION_HOOKS

--- src/helpers/Constants.sol
+++ src/helpers/Constants.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.20;
 // Index marking the start of the data for the validation function.
 uint8 constant RESERVED_VALIDATION_DATA_INDEX = type(uint8).max;
 
-// Maximum number of pre validation hooks that can be registered.
-uint8 constant MAX_PRE_VALIDATION_HOOKS = type(uint8).max;
+// Maximum number of validation-associated hooks that can be registered.
+uint8 constant MAX_VALIDATION_ASSOC_HOOKS = type(uint8).max;
 
 // Magic value for the Entity ID of direct call validation.
 uint32 constant DIRECT_CALL_VALIDATION_ENTITYID = type(uint32).max;
